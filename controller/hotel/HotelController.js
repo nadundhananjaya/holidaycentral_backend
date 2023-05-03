19c2512bfbad1f020175b6e3da7e1868c0b1159d
Samira Abayasekara <samiraabay@gmail.com>
2023-05-03 22:53:38 +0530

    added hotelController

---
+++ controller/hotel/HotelController.js
@@ -0,0 +1,59 @@
+import Hotel from "../../model/Hotel/Hotel.js";
+
+const requiredFields = [
+    "hotelName",
+    "starRating",
+    "address",
+    "city",
+    "state",
+    "postalCode",
+    "contact",
+    "lat",
+    "lng",
+    "facilities",
+    "rooms"
+]
+
+export const AddHotel = (req,res) => {
+    const missingFields = requiredFields.filter((field) => !req.body[field]);
+    if (missingFields.length) {
+        res.status(400).send({
+            message: `Missing required fields: ${missingFields.join(", ")}`,
+        });
+        return;
+    }
+    else{
+        const hotel = new Hotel({
+            hotelName : req.body.hotelName ,
+            starRating : req.body.starRating,
+            address : req.body.address,
+            city : req.body.city,
+            state : req.body.state,
+            postalCode : req.body.postalCode,
+            contact : req.body.contact,
+            lat : req.body.lat,
+            lng : req.body.lng,
+            facilities : req.body.facilities,
+            rooms : req.body.rooms
+        });
+
+        hotel.save().then(result => {
+            res.send(`${result} is successfully added !`)
+        }).catch(error => {
+            res.send(`${error}`)
+        })
+    }
+}
+
+export const HotelList = (req,res) => {
+    const filter = {
+        city : req.body.destination
+        // arrivalAirport :  req.body.arrivalAirport,
+    }
+    Hotel.find(filter).then(result => {
+        res.status(200).send(result)
+    }).catch(error => {
+        res.status(500).send(`Error ${error}`)
+    })
+}
+
